8f24686feecd67f4d8d88f369c23560133e94e3c
yashikabhatt1425 <yashikakbhatt@gmail.com>
2026-06-11 22:50:06 -0400

    Add questions with options and translations

---
+++ questions.js
@@ -0,0 +1,102 @@
+const questions = [
+{
+q: "I feel sleepy when I am ____.",
+options: ["Busy", "Alone", "Relaxed", "Excited"],
+map: {
+  "Busy": "vyast",
+  "Alone": "akele",
+  "Relaxed": "shaant",
+  "Excited": "utsahit"
+}
+},
+{
+q: "Memories come from ____.",
+options: ["Dreams", "Reality", "Past", "Future"],
+map: {
+  "Dreams": "khwabon",
+  "Reality": "haqeeqat",
+  "Past": "yaadon",
+  "Future": "bhavishya"
+}
+},
+{
+q: "A strong bond gives ____.",
+options: ["Support", "Friendship", "Love", "Respect"],
+map: {
+  "Support": "sahaara",
+  "Friendship": "dost",
+  "Love": "prem",
+  "Respect": "izzat"
+}
+},
+{
+q: "I think about you in ____.",
+options: ["Night", "Morning", "Dreams", "All time"],
+map: {
+  "Night": "raat",
+  "Morning": "savere",
+  "Dreams": "khwabon",
+  "All time": "hammesha"
+}
+},
+{
+q: "Distance feels ____.",
+options: ["Small", "Big", "Nothing", "Everything"],
+map: {
+  "Small": "chhota",
+  "Big": "mota",
+  "Nothing": "kuch nahi",
+  "Everything": "badhu"
+}
+},
+{
+q: "My heart chooses ____.",
+options: ["You", "Me", "Destiny", "Time"],
+map: {
+  "You": "tane",
+  "Me": "mane",
+  "Destiny": "kismat",
+  "Time": "samay"
+}
+},
+{
+q: "We connect through ____.",
+options: ["Words", "Silence", "Feelings", "Music"],
+map: {
+  "Words": "shabdo",
+  "Silence": "shanti",
+  "Feelings": "bhavnao",
+  "Music": "sangeet"
+}
+},
+{
+q: "I wait for ____.",
+options: ["Reply", "Call", "Message", "All"],
+map: {
+  "Reply": "javab",
+  "Call": "phone",
+  "Message": "sandesh",
+  "All": "badhu"
+}
+},
+{
+q: "You are my ____.",
+options: ["Friend", "Home", "Support", "All"],
+map: {
+  "Friend": "dost",
+  "Home": "ghar",
+  "Support": "sahaara",
+  "All": "badhu"
+}
+},
+{
+q: "This test is ____.",
+options: ["Real", "Fake", "Fun", "Strange"],
+map: {
+  "Real": "sacho",
+  "Fake": "khoto",
+  "Fun": "maja",
+  "Strange": "ajab"
+}
+}
+];
